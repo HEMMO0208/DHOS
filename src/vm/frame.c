b90f11867ecc527c0d116e8efcf157933bca6881
HEMMO0208 <gusah7787@postech.ac.kr>
2024-12-04 05:26:36 +0000

    aa

---
+++ src/vm/frame.c
@@ -0,0 +1,40 @@
+#include <frame.h>
+
+static struct list frame_table;
+static struct lock frame_lock;
+static struct list_elem *clock;
+
+void init_frame_table () 
+{
+    list_init(&frame_table);
+    lock_init(&frame_lock);
+    clock = NULL;
+}
+
+void insert_frame (struct frame* f)
+{
+    list_push_back(&frame_table, &f->elem);
+}
+
+void delete_frame (struct frame* f)
+{
+    struct list_elem *next = list_remove(&f->elem);
+
+    if (&f->elem == clock)
+        clock = next;
+}
+
+struct frame *find_frame (void *addr)
+{
+    struct list_elem *it = list_begin(&frame_table);
+    struct list_elem *end = list_end(&frame_table);
+
+    for (it; it != end; it = list_next(it)) {
+        struct frame *f  = list_entry(it, struct frame, elem);
+        if (f->page_addr == addr)
+            return f;
+    }
+
+    return NULL;
+}
+
--- src/vm/frame.h
+++ src/vm/frame.h
@@ -1,3 +1,6 @@
+#ifndef FRAME_H
+#define FRAME_H
+
 #include <list.h>
 #include <vm/page.h>
 #include <threads/vaddr.h>
@@ -7,4 +10,16 @@ struct frame{
     struct thread *t;
     struct list_elem elem;
     bool pinned;
-}
+};
+
+void init_frame_table();
+void insert_frame(struct frame* f);
+void delete_frame(struct frame* f);
+void free_frame(void *addr);
+
+struct frame *alloc_frame();
+struct frame *find_frame(void *addr);
+struct frame *find_frame_vaddr(void *vaddr);
+
+
+#endif
